68d2e546329e37d55c21d0ca0c2cb2f835a7fe75
armin <armin.oldboy@gmail.com>
2017-10-04 19:49:41 +0330

    wsgi changed

--- shop/wsgi.py
+++ shop/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for shop project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+path = '/home/pythonanywhereusername/projectname'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE']='projectname.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
