0dc14602ecf7c3b8c9e7a746c9213b39d584384b
armin <armin.oldboy@gmail.com>
2017-10-08 17:59:36 +0330

    debug True

--- shop/settings.py
+++ shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k22_gu(=m_@5waey%#f!4cqt3bx$%bk7rrkifty-@dxw*eh)v5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
